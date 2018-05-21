185768328581654f11db5e0bf1ad3a59bf00316a
joddiy <joddiy@qq.com>
2018-05-21 17:55:09 +0800

    commit

--- test.py
+++ test.py
@@ -28,25 +28,26 @@ def crop_exceed_data(data):
     return data[0: 8192]
 
 
-tmp_v = pd.read_csv("./input/test.csv", header=None, sep="|", names=['row_data'],
-                    error_bad_lines=False)
-tmp_v = tmp_v["row_data"].apply(lambda x: get_bytes_array(x))
-v_x = pd.DataFrame(tmp_v.tolist())
+# tmp_v = pd.read_csv("./input/test.csv", header=None, sep="|", names=['row_data'],
+#                     error_bad_lines=False)
+# tmp_v = tmp_v["row_data"].apply(lambda x: get_bytes_array(x))
+# v_x = pd.DataFrame(tmp_v.tolist())
 v_y = pd.read_csv("./input/test_label.csv", header=None, error_bad_lines=False)
-del tmp_v
-print('Shape of the v_x data: ', v_x.shape)
-print('Shape of the v_y data: ', v_y.shape)
+# del tmp_v
+# print('Shape of the v_x data: ', v_x.shape)
+# print('Shape of the v_y data: ', v_y.shape)
 
-model = load_model('./cache/b51f071681e13de00a641d77f6bf0046.h5')
+# model = load_model('./cache/b51f071681e13de00a641d77f6bf0046.h5')
 
 y_true = v_y
 fp_np_index = np.where(y_true == 0)
+y_pred = pd.read_csv("./input/y_pred.csv", header=None, error_bad_lines=False)
 
-y_pred = model.predict(v_x)
-sub = pd.DataFrame()
-sub['sample_id'] = range(len(y_pred))
-sub['malware'] = y_pred
-sub.to_csv('y_pred.csv', index=False)
+# y_pred = model.predict(v_x)
+# sub = pd.DataFrame()
+# sub['sample_id'] = range(len(y_pred))
+# sub['malware'] = y_pred
+# sub.to_csv('y_pred.csv', index=False)
 
 auc = roc_auc_score(y_true, y_pred)
 
