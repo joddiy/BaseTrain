b638d3be78d10585a5e29c2aad84bc24ceccda52
joddiy <joddiy@qq.com>
2018-05-24 17:21:02 +0800

    commit

--- src/test/t_auc_f.py
+++ src/test/t_auc_f.py
@@ -50,7 +50,7 @@ class TAUCF(Test):
 
             res = {}
 
-            for idx in range(100, 1000):
+            for idx in range(100, 501):
                 fp_np = y_pred[fp_np_index].shape[0]
                 thre_index = int(np.ceil(fp_np - fp_np * idx / 100000))
 
