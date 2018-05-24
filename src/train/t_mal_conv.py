b055fcb0f2db0cabfe2da41e1d9d07240f7b0d9d
joddiy <joddiy@qq.com>
2018-05-24 17:09:59 +0800

    commit

--- src/train/t_mal_conv.py
+++ src/train/t_mal_conv.py
@@ -138,7 +138,7 @@ class TMalConv(Train):
         tensor_board = TensorBoard(log_dir='./logs/' + self.p_md5, batch_size=batch_size)
         early_stopping = EarlyStopping("val_loss", patience=self.get_p("e_s_patience"), verbose=0, mode='auto')
         file_path = "./models/" + self.p_md5 + "-{epoch:04d}-{val_acc:.5f}.h5"
-        check_point = ModelCheckpoint(file_path, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+        check_point = ModelCheckpoint(file_path, monitor='val_loss', verbose=1, save_best_only=True, mode='auto')
         callbacks_list = [tensor_board, check_point]
 
         # Generators
