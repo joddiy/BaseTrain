52257e50774676cb41f83d656af0614880e41c43
joddiy <joddiy@qq.com>
2018-05-28 21:15:44 +0800

    commit

--- src/preprocess/pp_mal_conv.py
+++ src/preprocess/pp_mal_conv.py
@@ -164,7 +164,7 @@ class PPMalConv(PreProcess):
     def read_t_f(self):
         tmp_v = pd.read_csv(self.config["train"][0], header=None, sep="|", names=['row_data'],
                             error_bad_lines=False)
-        self.train = np.zeros((tmp_v.shape[0], 3584), dtype=int)
+        self.train = np.zeros((tmp_v.shape[0], 2048), dtype=int)
         self.label = pd.read_csv(self.config["label"][0], header=None, error_bad_lines=False)
         print('Shape of the train data: ', self.train.shape)
         print('Shape of the label data: ', self.label.shape)
@@ -172,8 +172,8 @@ class PPMalConv(PreProcess):
         for i, item in enumerate(tmp_v["row_data"]):
             # Store sample
             bytes_data = get_fixed_head(item)
-            if len(bytes_data) > 3584:
-                self.train[i, :] = bytes_data[:3584]
+            if len(bytes_data) > 2048:
+                self.train[i, :] = bytes_data[:2048]
             else:
                 self.train[i, 0:len(bytes_data)] = bytes_data
         return self.train, self.label
@@ -192,7 +192,7 @@ class PPMalConv(PreProcess):
     def read_v_f(self):
         tmp_v = pd.read_csv(self.config["v_train"][0], header=None, sep="|", names=['row_data'],
                             error_bad_lines=False)
-        self.v_x = np.zeros((tmp_v.shape[0], 8192), dtype=int)
+        self.v_x = np.zeros((tmp_v.shape[0], 2048), dtype=int)
         self.v_y = pd.read_csv(self.config["v_label"][0], header=None, error_bad_lines=False)
         print('Shape of the v_x data: ', self.v_x.shape)
         print('Shape of the v_y data: ', self.v_y.shape)
@@ -200,8 +200,8 @@ class PPMalConv(PreProcess):
         for i, item in enumerate(tmp_v["row_data"]):
             # Store sample
             bytes_data = get_fixed_head(item)
-            if len(bytes_data) > 3584:
-                self.train[i, :] = bytes_data[:3584]
+            if len(bytes_data) > 2048:
+                self.train[i, :] = bytes_data[:2048]
             else:
                 self.train[i, 0:len(bytes_data)] = bytes_data
 
